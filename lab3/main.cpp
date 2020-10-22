534344854ba5abf0169bee2a761c9e810111f523
Radosław Gąsior <radogas429@student.polsl.pl>
2020-10-22 13:08:54 +0200

    largest distance between points

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include<array>
+#include <array>
 #include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
@@ -15,8 +16,8 @@ struct point2d{
 
 point2d create_point() {
 	point2d temp;
-	temp.x = rand() % 100;
-	temp.y = rand() % 100;
+	temp.x = rand() % 10;
+	temp.y = rand() % 10;
 	return temp;
 }
 //pass by reference &&&&&&&&&&&
@@ -26,9 +27,42 @@ void create_in_place(point2d& point) {
 	point.y = rand() % 100;
 }
 
+float distance(point2d left, point2d right) {
+
+	return sqrt(pow((right.x - left.x), 2) + pow((right.y - left.y), 2));
+}
 
 int main() {
 
+	array<point2d, 10> points;
+
+	for (int i = 0; i < 10; i++) {
+		points[i] = create_point();
+		cout << "point[" << i << "] = (" << points[i].x << ", " << points[i].y << ")" << endl;
+	}
+
+	float largest_distance = 0.0;
+	point2d first, second;
+
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			float dist = distance(points[i], points[j]);
+
+			if (dist >= largest_distance) {
+				largest_distance = dist;
+				first = points[i];
+				second = points[j];
+			}
+
+			cout << dist << endl;
+		}
+	}
+
+	cout << "largest distance is:" << largest_distance << endl;
+	cout << "first point: " << first.x << "," << first.y << endl;
+	cout << "second point: " << second.x << "," << second.y << endl;
+
+	/*
 	//declare variable with struct
 	point2d mypoint=create_point();
 
@@ -36,11 +70,9 @@ int main() {
 
 	create_in_place(empty_point);
 
-
-
 	cout << "X value  " << empty_point.x << endl;
 	cout << "Y value  " << empty_point.y << endl;
-
+	*/
 
 
 	/*
